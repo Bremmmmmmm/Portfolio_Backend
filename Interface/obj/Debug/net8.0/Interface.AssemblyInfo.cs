d2e9cb25714fea5169227a368a4bfaaa2ec2b5c9
Bram Schouten <459133@student.fontys.nl>
2024-09-27 11:26:47 +0200

    docker test

--- Interface/obj/Debug/net8.0/Interface.AssemblyInfo.cs
+++ Interface/obj/Debug/net8.0/Interface.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8db839cb390cd3d51364ad05063ec73a60a7a8ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d2c1285c641cb666e535f0e94fb7cdd48fa7501")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
