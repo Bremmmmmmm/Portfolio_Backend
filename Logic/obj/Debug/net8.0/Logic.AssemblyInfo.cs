30ebd5dbaf2758d4362da179062cffefce424de2
Bram Schouten <459133@student.fontys.nl>
2024-09-27 11:36:09 +0200

    poging 3 docker

--- Logic/obj/Debug/net8.0/Logic.AssemblyInfo.cs
+++ Logic/obj/Debug/net8.0/Logic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d2c1285c641cb666e535f0e94fb7cdd48fa7501")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824eae8f3b1006ad149e9fc1dceb40c2a1c2198e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
